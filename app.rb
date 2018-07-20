436261615b349530830530cc7b18e07675c61056
Christine Cua <cloudycua@gmail.com>
2018-07-20 22:25:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -35,7 +35,8 @@ class App < Sinatra::Base
   end
 
   get '/fetch' do
-    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
+    "You did it! session[:foo] value: #{session[:foo]}.\n
+     Move on to Part II of this lab at '/second_exercise' "
   end
 
   get '/second_exercise' do
